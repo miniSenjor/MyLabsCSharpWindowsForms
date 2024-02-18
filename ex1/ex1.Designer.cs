df8cbcb0f2de845ba8785c5daa28253086e116ca
SystemX <skvorczov2005sergei@gmail.com>
2024-02-18 20:17:03 +0300

    начал 23

---
+++ ex1/ex1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace ex1
+{
+    partial class ex1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 94);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 128);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(217, 80);
+            this.txtArea.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(76, 20);
+            this.txtArea.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ширина";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(262, 174);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(76, 20);
+            this.txtHeight.TabIndex = 4;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(375, 128);
+            this.txtWidth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(76, 20);
+            this.txtWidth.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Высота";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 206);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Площадь";
+            // 
+            // ex1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ex1";
+            this.Text = "lr23ex1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ z1/z1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z1
+{
+    public partial class z1 : Form
+    {
+        public z1()
+        {
+            InitializeComponent();
+        }
+
+        List<Letters> letters = new List<Letters>();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int type;
+            if (rB1.Checked)
+                type = 1;
+            else if (rB2.Checked)
+                type = 2;
+            else
+                 type = 3;
+
+            Letters newLetter = new Letters(type, txtFrom.Text, txtTo.Text);
+            letters.Add(newLetter);
+            txtLettters.Text += newLetter.type;
+            txtFrom.Text = "";
+            txtTo.Text = "";
+        }
+    }
+
+    class EnterpriseDocument
+    {
+        int _type;
+        public EnterpriseDocument(int type)
+        {
+            this._type = type;
+        }
+
+        public int type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+    }
+
+    class Correspondent
+    {
+        string _fio;
+        public Correspondent( string fio )
+        {
+            this._fio = fio;
+        }
+
+        public string fio
+        {
+            get { return _fio; }
+            set { _fio = value; }
+        }
+    }
+
+    class Letters : EnterpriseDocument
+    {
+        string from;
+        string to;
+
+        public Letters(int type, string from, string to):
+        base(type)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public 
+    }
+}
