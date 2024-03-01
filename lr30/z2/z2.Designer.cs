0f82ce775588d73c6a13fe42cf5353196633488f
miniSenior <skvorczov2005sergei@gmail.com>
2024-03-01 20:59:31 +0300

    начал лр30 2

---
+++ lr30/z2/z2.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace z2
+{
+    partial class z2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rb3Side = new System.Windows.Forms.RadioButton();
+            this.rb2Side1Angle = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(238, 72);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(99, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // rb3Side
+            // 
+            this.rb3Side.AutoSize = true;
+            this.rb3Side.Checked = true;
+            this.rb3Side.Location = new System.Drawing.Point(240, 18);
+            this.rb3Side.Name = "rb3Side";
+            this.rb3Side.Size = new System.Drawing.Size(97, 21);
+            this.rb3Side.TabIndex = 2;
+            this.rb3Side.TabStop = true;
+            this.rb3Side.Text = "3 стороны";
+            this.rb3Side.UseVisualStyleBackColor = true;
+            this.rb3Side.CheckedChanged += new System.EventHandler(this.rb3Side_CheckedChanged);
+            // 
+            // rb2Side1Angle
+            // 
+            this.rb2Side1Angle.AutoSize = true;
+            this.rb2Side1Angle.Location = new System.Drawing.Point(240, 45);
+            this.rb2Side1Angle.Name = "rb2Side1Angle";
+            this.rb2Side1Angle.Size = new System.Drawing.Size(141, 21);
+            this.rb2Side1Angle.TabIndex = 3;
+            this.rb2Side1Angle.Text = "2 стороны и угол";
+            this.rb2Side1Angle.UseVisualStyleBackColor = true;
+            this.rb2Side1Angle.CheckedChanged += new System.EventHandler(this.rb2Side1Angle_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(100, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Сторона 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Сторона 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Сторона 3";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(12, 135);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(369, 98);
+            this.txtInfo.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Инфа о треугольнике:";
+            // 
+            // z2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 245);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.rb2Side1Angle);
+            this.Controls.Add(this.rb3Side);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "z2";
+            this.Text = "lr30z2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton rb3Side;
+        private System.Windows.Forms.RadioButton rb2Side1Angle;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lr30/z2/z2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z2
+{
+    public partial class z2 : Form
+    {
+        public z2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double side1, side2, side3, angle1, angle2, angle3, area;
+                side1 = double.Parse(textBox1.Text);
+                side2 = double.Parse(textBox2.Text);
+                if (rb3Side.Checked)
+                {
+                    side3 = double.Parse(textBox3.Text);
+                    angle1 = calcAngle(side2, side3, side1);
+                    angle2 = calcAngle(side1, side3, side2);
+                    angle3 = 180 - angle1 - angle2;
+                }
+                else
+                {
+                    angle3 = double.Parse(textBox3.Text);
+                    side3 = side1 * side1 + side2 * side2 - 2 * side1 * side2 * Math.Cos(angle3);
+                    angle1 = calcAngle(side2, side3, side1);
+                    angle2 = 180 - angle1 - angle3;
+                }
+                area = side1 * side2 * Math.Sin(angle3)/2;
+                txtInfo.Text = $"Длинны сторон: {side1} {side2} {side3}\r\n" +
+                    $"Углы: {angle1} {angle2} {angle3}\r\n" +
+                    $"Площадь: {area}";
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка ввода данных");
+            }
+        }
+
+        double calcAngle(double s1, double s2, double s3)
+        {
+            return Math.Acos((s1*s1+s2*s2-s3*s3)/(2*s1*s2));
+        }
+
+        private void rb3Side_CheckedChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Сторона 3";
+        }
+
+        private void rb2Side1Angle_CheckedChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Угол";
+        }
+    }
+}
